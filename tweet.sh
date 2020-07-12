068aa124bb3497af1705cd3b8bcd22f22b405a84
Bruno Minossi <b.minossi@gmail.com>
2020-07-11 21:38:25 -0300

    putting a lot of respect with identation

--- tweet.sh
+++ tweet.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-if [ ! -f "lastMonitTwitter" ];then
-touch lastMonitTwitter
+if [ ! -f "lastMonitTwitter" ]; then
+  touch lastMonitTwitter
 fi
 monitoredTwitterUser="HusseiN98D"
 
-result=`
-curl -s \
--H "X-Requested-With:XMLHttpRequest" \
--H "X-Twitter-Active-User:yes;User-Agent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8" \
-"https://twitter.com/i/profiles/show/$monitoredTwitterUser/timeline/tweets?include_available_features=1&include_entities=1&include_new_items_bar=true" 
-`
-jq=`echo $result | grep -o '{"min_position":.*'`
+result=$(
+  curl -s \
+    -H "X-Requested-With:XMLHttpRequest" \
+    -H "X-Twitter-Active-User:yes;User-Agent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8" \
+    "https://twitter.com/i/profiles/show/$monitoredTwitterUser/timeline/tweets?include_available_features=1&include_entities=1&include_new_items_bar=true"
+)
+jq=$(echo $result | grep -o '{"min_position":.*')
 
-#totalPosts=`echo "$jq" | jq .new_latent_count`
-echo $totalPosts
+#totalPosts=`echo "$jq" | jq .new_latent_count` //I will not use it, but maybe can help you
+
 echo -e "Ultimo post feito:"
-echo -e $jq | grep -v "Pinned" | grep -m1 "data-aria-label-part...0.*" | tr -d "\\" 2>/dev/null | html2text 
-
+echo -e $jq | grep -v "Pinned" | grep -m1 "data-aria-label-part...0.*" | tr -d "\\" 2>/dev/null | html2text
