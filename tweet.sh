fb578daeb1e22efe8ec20f44b036cdc9abdac5bc
Bruno Minossi <b.minossi@gmail.com>
2020-07-11 21:24:59 -0300

    Breaking line

--- tweet.sh
+++ tweet.sh
@@ -5,7 +5,10 @@ fi
 monitoredTwitterUser="HusseiN98D"
 
 result=`
-curl -s -H "X-Requested-With:XMLHttpRequest" -H "X-Twitter-Active-User:yes;User-Agent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8" "https://twitter.com/i/profiles/show/$monitoredTwitterUser/timeline/tweets?include_available_features=1&include_entities=1&include_new_items_bar=true" 
+curl -s \
+-H "X-Requested-With:XMLHttpRequest" \
+-H "X-Twitter-Active-User:yes;User-Agent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8" \
+"https://twitter.com/i/profiles/show/$monitoredTwitterUser/timeline/tweets?include_available_features=1&include_entities=1&include_new_items_bar=true" 
 `
 jq=`echo $result | grep -o '{"min_position":.*'`
 
