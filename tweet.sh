945315050453014f8f4c50c025de5d484b14fe64
Bruno Minossi <b.minossi@gmail.com>
2020-07-12 15:44:44 -0300

    Check if all dependencies libraries/ jq and html2text is installed

--- tweet.sh
+++ tweet.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
 listTwitterUsersToCheck=("CyberResenha" "avanish46")
+components=("jq" "html2text")
 
 function dependencies() {
-  #Check jq | html2text
+
+  for component in "${components[@]}"; do
+    command -v $component > /dev/null 2>&1
+    if [ "$?" -eq 1 ]; then
+      echo -e "[Check Dependencies Failed]\nConsider to sudo apt install $component"
+      exit
+    fi
+  done
+
   if [ ! -f "$userToCheck" ]; then
     touch $userToCheck
   fi
+
   ConfirmIfallParamsAreFilled=$(cat configurations | cut -d "=" -f2 | sed '/^$/d' | wc -l)
-  if [[ "$ConfirmIfallParamsAreFilled" -eq 3 ]] ; then
+
+  if [[ "$ConfirmIfallParamsAreFilled" -eq 3 ]]; then
     source configurations
-    else
-    echo "You didnt fill the configuration file or you've deleted it, so you have to recreate it";
+  else
+    echo "You didnt fill the configuration file or you've deleted it, so you have to recreate it"
   fi
 }
 
@@ -33,49 +44,49 @@ function fetchLastTwoTweetsFromUserMonitored() {
       # Fixing images url
       sed 's#pic.twitter# http://pic.twitter#g'
   )
-    someNewToSendByTelegram $lastTwoTweetsFetched
+  someNewToSendByTelegram $lastTwoTweetsFetched
 }
 
 function someNewToSendByTelegram() {
   lastTwoTweetsStoredMd5=($(cat $userToCheck))
   lastTwoTweetsFetchedMd5=($(echo $lastTwoTweetsFetched | md5sum))
 
-  if [ "$lastTwoTweetsStoredMd5" == "$lastTwoTweetsFetchedMd5" ];then
+  if [ "$lastTwoTweetsStoredMd5" == "$lastTwoTweetsFetchedMd5" ]; then
     echo "Nothing new to $userToCheck"
     return
-    fi
-    echo "There are some news to $userToCheck [$lastTwoTweetsStoredMd5/$lastTwoTweetsFetchedMd5]! Logging and Sending to telegram channel"
-    updatingLastTweetsFromUser $lastTwoTweetsFetchedMd5
-    sendNewsToTelegramChannel $lastTwoTweetsFetched
+  fi
+  echo "There are some news to $userToCheck [$lastTwoTweetsStoredMd5/$lastTwoTweetsFetchedMd5]! Logging and Sending to telegram channel"
+  updatingLastTweetsFromUser $lastTwoTweetsFetchedMd5
+  sendNewsToTelegramChannel $lastTwoTweetsFetched
 }
 
-function updatingLastTweetsFromUser(){
-  echo $lastTwoTweetsFetchedMd5 > $userToCheck
+function updatingLastTweetsFromUser() {
+  echo $lastTwoTweetsFetchedMd5 >$userToCheck
 }
 
-function sendNewsToTelegramChannel(){
+function sendNewsToTelegramChannel() {
   echo "Function to send news to telegram channel"
   encodedNewTweetUpdate=$(urlencode $lastTwoTweetsFetched)
-  telegramUrlToSendContent=$apiUrlTelegram"sendmessage?chat_id="$chatID"&text=%5B%40$userToCheck%5D"$encodedNewTweetUpdate;
+  telegramUrlToSendContent=$apiUrlTelegram"sendmessage?chat_id="$chatID"&text=%5B%40$userToCheck%5D"$encodedNewTweetUpdate
   echo "Fazendo curl para [$telegramUrlToSendContent]"
-	curl -s $telegramUrlToSendContent;
+  curl -s $telegramUrlToSendContent
 }
 
-function urlencode(){
+function urlencode() {
   local string="$lastTwoTweetsFetched"
   local strlen=${#string}
   local encoded=""
   local pos c o
 
-  for (( pos=0 ; pos<strlen ; pos++ )); do
-     c=${string:${pos}:1}
-     case "$c" in
-        [-_.~a-zA-Z0-9] ) o="${c}" ;;
-        * )               printf -v o '%%%02x' "'$c"
-     esac
-     encoded+="${o}"
+  for ((pos = 0; pos < strlen; pos++)); do
+    c=${string:${pos}:1}
+    case "$c" in
+    [-_.~a-zA-Z0-9]) o="${c}" ;;
+    *) printf -v o '%%%02x' "'$c" ;;
+    esac
+    encoded+="${o}"
   done
-  echo "${encoded}"    # You can either set a return variable (FASTER)
+  echo "${encoded}" # You can either set a return variable (FASTER)
 }
 
 function main() {
