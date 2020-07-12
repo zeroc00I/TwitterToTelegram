c879b218032ebf1f210f73b74db750a919d81a88
Bruno Minossi <b.minossi@gmail.com>
2020-07-12 02:44:52 -0300

    Refactoring to look like more a real O.O script

--- tweet.sh
+++ tweet.sh
@@ -1,18 +1,59 @@
 #!/bin/bash
-if [ ! -f "lastMonitTwitter" ]; then
-  touch lastMonitTwitter
-fi
-monitoredTwitterUser="HusseiN98D"
+listTwitterUsersToCheck=("CyberResenha" "avanish46")
 
-result=$(
-  curl -s \
-    -H "X-Requested-With:XMLHttpRequest" \
-    -H "X-Twitter-Active-User:yes;User-Agent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8" \
-    "https://twitter.com/i/profiles/show/$monitoredTwitterUser/timeline/tweets?include_available_features=1&include_entities=1&include_new_items_bar=true"
-)
-jq=$(echo $result | grep -o '{"min_position":.*')
+function dependencies() {
+  #Check jq | html2text
+  if [ ! -f "$userToCheck" ]; then
+    touch $userToCheck
+  fi
+}
 
-#totalPosts=`echo "$jq" | jq .new_latent_count` //I will not use it, but maybe can help you
+function someNewToSendByTelegram() {
+  lastTwoTweetsStoredMd5=($(cat $userToCheck | md5sum))
+  lastTwoTweetsFetchedMd5=($(echo $userToCheck | md5sum))
 
-echo -e "Ultimo post feito:"
-echo -e $jq | grep -v "Pinned" | grep -m1 "data-aria-label-part...0.*" | tr -d "\\" 2>/dev/null | html2text
+  if [ "$lastTwoTweetsStoredMd5" == "$lastTwoTweetsFetchedMd5" ];then
+    echo "Nothing new to $userToCheck"
+    exit
+    fi
+    echo "There are some news! Logging and Sending to telegram channel"
+    updatingLastTweetsFromUser $lastTwoTweetsFetchedMd5
+    sendNewsToTelegramChannel $lastTwoTweetsFetched
+}
+
+function fetchLastTwoTweetsFromUserMonitored() {
+  resultOfFetch=$(
+    curl -s \
+      -H "X-Requested-With:XMLHttpRequest" \
+      -H "X-Twitter-Active-User:yes;User-Agent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8" \
+      "https://twitter.com/i/profiles/show/$userToCheck/timeline/tweets?include_available_features=1&include_entities=1&include_new_items_bar=true"
+  )
+
+  preparingJsonToReadable=$(echo $resultOfFetch | grep -o '{"min_position":.*')
+  lastTwoTweetsFetched=$(
+    echo -e $preparingJsonToReadable | grep -m2 "data-aria-label-part...0.*" |
+      # Removing unnecessary escapes
+      tr -d "\\" 2>/dev/null |
+      # removing html tags
+      html2text |
+      # Fixing images url
+      sed 's#pic.twitter# http://pic.twitter#g'
+  )
+    someNewToSendByTelegram $lastTwoTweetsFetched
+}
+
+function updatingLastTweetsFromUser(){
+  echo $lastTwoTweetsFetchedMd5 > $userToCheck
+}
+function sendNewsToTelegramChannel(){
+  echo "Function to send news to telegram channel"
+}
+
+function main() {
+  dependencies $userToCheck
+  fetchLastTwoTweetsFromUserMonitored $userToCheck
+}
+
+for userToCheck in "${listTwitterUsersToCheck[@]}"; do
+  main $userToCheck
+done
